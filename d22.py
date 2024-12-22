c7e76694dc65073efaa0989eae359fff56904aee
Joao Salavisa <joao.salavisa@gmail.com>
2024-12-22 22:04:17 +0000

    Day 22 cleanup

--- d22.py
+++ d22.py
@@ -2,21 +2,18 @@ from collections import defaultdict
 
 lines = [ int(line) for line in open('d22.in').readlines() ]
 
+def mix(a, b):
+    return a ^ b
+
+def prune(a):
+    return a % 16777216
+
 def simulate_secrets(secret, iterations):
     secrets = []
     for _ in range(iterations):
-        v1 = secret * 64
-        secret = secret ^ v1
-        secret = secret % 16777216
-
-        v2 = secret // 32
-        secret = secret ^ v2
-        secret = secret % 16777216
-
-        v3 = secret * 2048
-        secret = secret ^ v3
-        secret = secret % 16777216
-
+        secret = prune(mix(secret, secret * 64))
+        secret = prune(mix(secret, secret // 32))
+        secret = prune(mix(secret, secret * 2048))
         secrets.append(secret)
     return secrets
 
@@ -31,24 +28,17 @@ def part2():
         prices = [s % 10 for s in simulate_secrets(secret, 2000)]
         changes = [b - a for a, b in zip(prices[:-1], prices[1:])]
 
-        window = []
-        seen = {}
-        for i in range(len(changes)):
-            window.append(changes[i])
-            if len(window) == 4:
-                sequence = tuple(window)
-                if sequence not in seen:
-                    banana = prices[i+1]  
-                    bananas[sequence] += banana
-                    seen[sequence] = True
-                window.pop(0)
+        seen = set()
+        max_value = 0
+        for i in range(len(changes) - 3):
+            sequence = tuple(changes[i:i+4])
+            if sequence not in seen:
+                bananas[sequence] += prices[i+4]
+                if bananas[sequence] > max_value:
+                    max_value = bananas[sequence]
+                seen.add(sequence)
 
-    max = 0
-    for total_banana in bananas.values():
-        if total_banana > max:
-            max = total_banana
-
-    print(max)
+    print(max_value)
 
 part1()
 part2()
